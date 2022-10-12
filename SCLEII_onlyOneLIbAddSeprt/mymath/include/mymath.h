23d8fcee620ad0d925b1c6014409298c3ceadfdd
leo <leoking02@foxmail.com>
2022-10-12 09:04:06 +0800

    first commit    aaaa

---
+++ SCLEII_onlyOneLIbAddSeprt/mymath/include/mymath.h
@@ -0,0 +1,8 @@
+#ifndef MYMATH_H
+#define MYMATH_H
+
+int add(int, int);
+
+double add(double, double);
+
+#endif
---
+++ SCLEII_onlyOneLIbAddSeprt_19.0.24215.1_install/lib/cmake/mymath/MyMathTargets-debug.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Debug".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "MyMath::mymath" for configuration "Debug"
+set_property(TARGET MyMath::mymath APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(MyMath::mymath PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/lib/mymathd.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/mymathd.dll"
+  )
+
+list(APPEND _cmake_import_check_targets MyMath::mymath )
+list(APPEND _cmake_import_check_files_for_MyMath::mymath "${_IMPORT_PREFIX}/lib/mymathd.lib" "${_IMPORT_PREFIX}/bin/mymathd.dll" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
